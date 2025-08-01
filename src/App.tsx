4c2f4fc87386ba0474ddd17aac18387d4deb9026
krishna-123-star <prabhadevi2505@gmail.com>
2025-08-01 16:50:11 +0530

    Button and its Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import UserProfile from "./UserProfile"
+import Button from "./Button";
+import UserProfile from "./UserProfile";
 
+const handleClick = () => {
+  alert("Clicked me");
+};
 
 function App() {
   return (
-<div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
-      <UserProfile />
+    <div
+      style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
+    >
+      {/* <UserProfile /> */}
+      <Button label="Click Me" onClick={handleClick} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
