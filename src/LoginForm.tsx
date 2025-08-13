307d906ed23c39aaf90168da2bee6732ddb8c128
krishna-123-star <prabhadevi2505@gmail.com>
2025-08-13 17:36:07 +0530

    Using object

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,27 +1,37 @@
-import { useState, type ChangeEvent, type FormEvent } from "react";
+import React, { useState, type ChangeEvent, type FormEvent } from "react";
 
-function useFormInput(initialValue: string) {
-  const [value, setValue] = useState(initialValue);
+type FormValues = {
+  [key: string]: string;
+};
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+function useForm(initialValues: FormValues) {
+  const [formData, setFormData] = useState<FormValues>(initialValues);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
   return {
-    value,
-    onChange,
+    formData,
+    handleChange,
   };
 }
 
 function LoginForm() {
-  const username = useFormInput("");
-  const password = useFormInput("");
+  const { formData, handleChange } = useForm({
+    username: "",
+    password: "",
+  });
   const [message, setMessage] = useState("");
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (username.value === "admin" && password.value === "xminds@123") {
+    if (formData.username === "admin" && formData.password === "xminds@123") {
       setMessage("Login successful");
     } else {
       setMessage("Try again!!");
@@ -32,9 +42,21 @@ function LoginForm() {
     <div>
       <h2>Login Form</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Username" {...username} />
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder="Username"
+        />
         <br />
-        <input type="password" placeholder="Password" {...password} />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Password"
+        />
         <br />
         <button type="submit">Login</button>
       </form>
