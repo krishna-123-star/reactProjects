84aa8d7a9bafe2a55c49b77b1c6ce87f9a4ab1d9
krishna-123-star <prabhadevi2505@gmail.com>
2025-09-03 16:14:27 +0530

    Lazy Loading and Protected Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import { Routes, Route } from "react-router-dom";
-import Products from "./Products";
-import ProductDetail from "./api/ProductDetail";
+import { Suspense, lazy } from "react";
+import { AuthProvider } from "./api/AuthContext";
+import ProtectedRoute from "./ProtectedRoute";
+
+const LoginForm = lazy(() => import("./LoginForm"));
+const Products = lazy(() => import("./Products"));
+const ProductDetail = lazy(() => import("./api/ProductDetail"));
 
 function App() {
   return (
-    <Routes>
+    <AuthProvider>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Routes>
+            <Route path="/login" element={<LoginForm />} />
 
-      <Route path="/products" element={<Products />}>
-        <Route path=":id" element={<ProductDetail />} />
-      </Route>
-    </Routes>
+            <Route
+              path="/products"
+              element={
+                <ProtectedRoute>
+                  <Products />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/products/:id"
+              element={
+                <ProtectedRoute>
+                  <ProductDetail />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Default redirect */}
+            <Route path="*" element={<LoginForm />} />
+          </Routes>
+        </Suspense>
+    </AuthProvider>
   );
 }
 
--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,38 +1,34 @@
 import React, { useState, type ChangeEvent, type FormEvent } from "react";
+import { useAuth } from "./api/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 type FormValues = {
-  [key: string]: string;
+  username: string;
+  password: string;
 };
 
-function useForm(initialValues: FormValues) {
-  const [formData, setFormData] = useState<FormValues>(initialValues);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  return {
-    formData,
-    handleChange,
-  };
-}
-
 function LoginForm() {
-  const { formData, handleChange } = useForm({
+  const [formData, setFormData] = useState<FormValues>({
     username: "",
     password: "",
   });
   const [message, setMessage] = useState("");
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (formData.username === "admin" && formData.password === "xminds@123") {
+      const fakeToken = "asdf1234token";
+      setToken(fakeToken);
       setMessage("Login successful");
+      navigate("/products");
     } else {
       setMessage("Try again!!");
     }
